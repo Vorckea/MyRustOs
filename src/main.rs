623f4214e807aa14df677263e7b860a996321875
Vorckea <akselschrader@gmail.com>
2022-09-14 21:47:37 +0200

    Os

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+//cargo build --target thumbv7em-none-eabihf
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
